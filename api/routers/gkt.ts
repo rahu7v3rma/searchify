935a6dc5e201596ec24aea9f14ae0b65d5e6ffce
rahu7v3rma <rahu7v3rma@gmail.com>
2025-05-03 23:19:43 +0530

    refactor api and frontend

--- api/routers/gkt.ts
+++
@@ -1,5 +0,0 @@
-import express from "express";
-
-const GKTRouter = express.Router();
-
-export default GKTRouter;
--- api/validators/admin.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const AdminContactUsSchema = z.object({
-  email: z.string().email({ message: "Invalid email address" }),
-  message: z.string().min(1, { message: "Message is required" }),
-});
