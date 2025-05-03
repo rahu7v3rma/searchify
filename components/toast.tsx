c87d8835fa6277fe27406de53cb56a93ab470e77
rahu7v3rma <rahu7v3rma@gmail.com>
2025-05-04 00:43:09 +0530

    setup toast component

---
+++ components/toast.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React from "react";
+import useToast from "../hooks/toast";
+
+export default function Toast() {
+  const { toasts } = useToast();
+
+  if (!toasts.length) return null;
+
+  return (
+    <div className="fixed top-[100px] right-4 z-50 space-y-2">
+      {toasts.map((toast) => (
+        <div
+          key={toast.id}
+          className="bg-primary-toast-background text-white px-4 py-2 rounded-lg shadow-lg max-w-sm"
+        >
+          {toast.message}
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ context/toast.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { createContext, ReactNode, useState, useEffect } from "react";
+
+type Toast = {
+  id: number;
+  message: string;
+  show: boolean;
+};
+
+export const ToastContext = createContext<{
+  toasts: Toast[];
+  showToast: (message: string) => void;
+}>({
+  toasts: [],
+  showToast: () => {},
+});
+
+export default function ToastProvider({ children }: { children: ReactNode }) {
+  const [toasts, setToasts] = useState<Toast[]>([]);
+
+  const showToast = (message: string) => {
+    const id = Date.now();
+    const newToast = { id, message, show: true };
+
+    setToasts((prev) => [...prev, newToast]);
+
+    setTimeout(() => {
+      setToasts((prev) => prev.filter((toast) => toast.id !== id));
+    }, 3000);
+  };
+
+  return (
+    <ToastContext.Provider value={{ toasts, showToast }}>
+      {children}
+    </ToastContext.Provider>
+  );
+}
